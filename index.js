d3e5ffc7f32e058bfea32d287083ae7118f3a393
Juliana <juliana.silva@prod.ag>
2023-11-24 13:13:33 -0300

    refactor

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
@@ -7,10 +7,22 @@ console.log(ulEl)
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = ""
+    renderLeads()
 })
 
 
-for (let i = 0; i < myLeads.length; i++) {
-  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+function renderLeads() {
+  let listItems = ""
+  for (let i = 0; i < myLeads.length; i++) {
+      listItems += `
+          <li>
+              <a target='_blank' href='${myLeads[i]}'>
+                  ${myLeads[i]}
+              </a>
+          </li>
+      `
+  }
+  ulEl.innerHTML = listItems  
 }
+
